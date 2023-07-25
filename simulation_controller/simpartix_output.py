6d75294d9fd957381c938ddf54dc2312ce75c1a0
nash <62147380+yoavnash@users.noreply.github.com>
2023-07-25 10:21:56 +0200

    Remove times

--- simulation_controller/simpartix_output.py
+++ simulation_controller/simpartix_output.py
@@ -2,8 +2,7 @@
 
 
 class SimPARTIXOutput:
-    def __init__(self, temperature, group, state_of_matter, times):
-        self.times = times
+    def __init__(self, temperature, group, state_of_matter):
         self.temperature = temperature
         self.group = group
         self.state_of_matter = state_of_matter
